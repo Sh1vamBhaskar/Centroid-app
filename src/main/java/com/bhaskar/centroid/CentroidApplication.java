c21ee21ef614aade876379c7ebb2b5a074afc51e
Sh1vamBhaskar <144939839+Sh1vamBhaskar@users.noreply.github.com>
2026-08-06 04:21:22 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/bhaskar/centroid/CentroidApplication.java
@@ -0,0 +1,13 @@
+package com.bhaskar.centroid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentroidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentroidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhaskar/centroid/CentroidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhaskar.centroid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentroidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
